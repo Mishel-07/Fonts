aa82e2084a7fbf23c33c5dae7224761cedcc588b
Mishal <131391958+Mishel-07@users.noreply.github.com>
2025-05-15 18:36:03 +0530

    Update font.py

--- font.py
+++ font.py
@@ -1,5 +1,20 @@
 import requests
-import json
 
-response = requests.get("https://raw.githubusercontent.com/Mishel-07/Fonts/refs/heads/main/fonts.json")
-print(response.json())
+
+def font(text, style):
+    try:
+        response = requests.get("https://raw.githubusercontent.com/Mishel-07/Fonts/main/fonts.json")
+        fonts = response.json()
+
+        if style not in fonts:
+            return "style not found"
+
+        data = fonts[style]
+        result = ""
+
+        for l in text:
+            result += data.get(l, l)  
+        return result
+
+    except Exception as e:
+        return f"error: {str(e)}"
